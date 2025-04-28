91b8e3fd266489731f4ca62d411d7119dc8a2f51
Cameron Madlin <lumpycam@comcast.net>
2025-04-27 21:40:46 -0700

    Update and rename problem5 to problem5.py
    Added problem prompt as a comment on line 1

---
+++ Mod3/problem5.py
@@ -0,0 +1,6 @@
+#5. Print 3 words with a | character (called a pipe) in between them. Use the appropriate keyword argument in print() to do so.
+word_1 = "Hello"
+word_2 = "I"
+word_3 = "am"
+word_4 = "Cameron"
+print(word_1, word_2, word_3, word_4, sep="|")
