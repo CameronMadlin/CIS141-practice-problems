fb11d4ce2ed4a0d8d765adf289f20652937efa18
Cameron Madlin <lumpycam@comcast.net>
2025-04-27 21:39:12 -0700

    Update and rename problem2 to problem2.py
    Added problem prompt as a comment on the first line

---
+++ Mod3/problem2.py
@@ -0,0 +1,5 @@
+#2. Prompt the user for their name and their age. Calculate their age next year. Use string concatenation to print "Hello, <name>! You are <age1> years old. Next year, you will be <age2> years old."
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+age2 = age + 1
+print(f"Hello, {name}! You are {age} years old. Next year, you will be {age2}!")
