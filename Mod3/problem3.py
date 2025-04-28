a130e91b441a1f58d69a36597d7bca2903245b89
Cameron Madlin <lumpycam@comcast.net>
2025-04-27 21:39:42 -0700

    Update and rename problem3 to problem3.py
    Added problem prompt as a comment on the first line

---
+++ Mod3/problem3.py
@@ -0,0 +1,4 @@
+#3. Prompt the user for a sentence and a word to try to find in that sentence. Have the program print out whether the word was found in the sentence. (i.e. True or False)
+sentance = input("What sentance do you want to use? ")
+word = input("What word are you looking for? ")
+print(word in sentance)
