1ad9bf661d4abf83f49d351e9cfea3260ce21e99
Cameron Madlin <lumpycam@comcast.net>
2025-04-27 21:40:12 -0700

    Update and rename problem4 to problem4.py
    Added problem prompt as a comment on the first line

--- Mod3/problem4.py
+++ Mod3/problem4.py
@@ -1,3 +1,4 @@
+#4. Prompt the user for: a word, a first index, and a last index. Slice the word at the indexes provided by the user and print to the screen.
 word = input("What word do you want to use? ")
 index1 = int(input("What is the start of the index? "))
 index2 = int(input("What is the end of the index? "))
